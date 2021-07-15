ae8579edde5d71816697efcc6dcaeaa49560b8ad
MrCarb0n <Tiash97@yahoo.com>
2021-07-16 02:55:28 +0600

    KryptoKernel!
     - 4.9 compatibility checker
     - use original boot path
     - i/o sched prop nuked
     - banner & useless strings added
    
    Signed-off-by: MrCarb0n <Tiash97@yahoo.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,77 +4,42 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=|               VV • YYYYMMDD • HHMM               |
 do.devicecheck=1
-do.modules=0
-do.systemless=1
 do.cleanup=1
-do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
-supported.versions=
-supported.patchlevels=
-'; } # end properties
+do.cleanuponabort=1
+device.name1=mido
+supported.versions=10-11
+supported.patchlevels=2021-01-
+'; }
+# end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/platform/soc/7824900.sdhci/by-name/boot;
 is_slot_device=0;
+no_block_display=0;
 ramdisk_compression=auto;
 
-
 ## AnyKernel methods (DO NOT CHANGE)
-# import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
+## Check kernel base compatibility for 4.9-mido //
+ui_print " " "| - Checking OS kernel compatibility..             |";
+if [ ! -f /vendor/etc/init/hw/init.msm.usb.configfs.rc ]; then
+ui_print "    Incompatible OS, installation aborted!"; exit 1;
+fi; ui_print "    Requirement reached.";
 
 ## AnyKernel boot install
 dump_boot;
 
-# begin ramdisk changes
-
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
-
-# end ramdisk changes
-
 write_boot;
 ## end boot install
 
-
-# shell variables
-#block=vendor_boot;
-#is_slot_device=1;
-#ramdisk_compression=auto;
-
-# reset for vendor_boot patching
-#reset_ak;
-
-
-## AnyKernel vendor_boot install
-#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
-
-#flash_boot;
-## end vendor_boot install
-
+## remove predefined i/o scheduler properties if available by @MrCarb0n
+PBP=/system/product/build.prop;
+PROP=persist.sys.io.scheduler;
+if [ "$(grep -c $PROP $PBP)" == "1" ]; then
+mount -o rw,remount -t auto /system >/dev/null;
+restore_file $PBP;
+backup_file $PBP;
+remove_line $PBP $PROP; fi;
